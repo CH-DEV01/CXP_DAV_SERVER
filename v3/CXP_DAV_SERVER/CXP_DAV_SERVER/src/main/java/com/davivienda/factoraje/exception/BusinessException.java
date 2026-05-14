37632db75cc0f43b1d69076bed0f0ecf05627074
CARLOS HERNANDEZ <chernandez-27@outlook.com>
2026-05-13 23:08:59 -0600

    delete

--- v3/CXP_DAV_SERVER/CXP_DAV_SERVER/src/main/java/com/davivienda/factoraje/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.davivienda.factoraje.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- v3/CXP_DAV_SERVER/CXP_DAV_SERVER/src/main/java/com/davivienda/factoraje/repository/HandoffRepository.java
+++
@@ -1,7 +0,0 @@
-package com.davivienda.factoraje.repository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class HandoffRepository {
-    
-}
